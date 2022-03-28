e7a92a7e813f0117230cd8caf291c66d59a88bb5
Bahadır Koramaz <bkork1maz@gmail.com>
2022-03-28 23:29:48 +0300

    28.03.22

--- src/day_29staticKeyword/C01_Static.java
+++ src/day_29staticKeyword/C01_Static.java
@@ -1,2 +1,18 @@
-package day_29staticKeyword;public class C01_Static {
+package day_29staticKeyword;
+
+public class C01_Static {
+
+    String okulİsmi="Yildiz Koleji";
+    static String okulTelefonu="3122563635";
+
+    public static void staticMethod(){
+
+        System.out.println("Statik Method calişti");
+    }
+
+    public static void staticOlmayanMethod(){
+
+        System.out.println("Static olmayan method calişti");
+    }
+
 }
--- src/day_29staticKeyword/C03.java
+++ src/day_29staticKeyword/C03.java
@@ -1,2 +1,11 @@
-package day_29staticKeyword;public class C03 {
+package day_29staticKeyword;
+
+public class C03 {
+
+    static int sayi=5;
+
+    public static void artirma(){
+        sayi++;
+        System.out.println("sayının C03 deki değeri");
+    }
 }
