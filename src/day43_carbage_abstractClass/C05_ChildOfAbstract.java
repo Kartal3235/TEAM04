9bd49cc691fab72363c2c6430aee6d47801a1a54
Bahadır Koramaz <bkork1maz@gmail.com>
2022-04-13 19:20:10 +0300

    13.04.22

---
+++ src/day43_carbage_abstractClass/C05_ChildOfAbstract.java
@@ -0,0 +1,17 @@
+package day43_carbage_abstractClass;
+
+public class C05_ChildOfAbstract extends C04{
+
+    /*
+    Classımızı bir Abstract class ın child classı olarak belirtmek istersek
+    Java hemen class isminin altını kırmızı cizer
+
+    bizden
+    child classı abstract yapmamız veya parent class daki abstract methodları override etmemizi
+    ister
+
+    Parent olan abstract classdaki concrete methodlar ise opsiyoneldir.
+    onları istersek override ederiz,istemezsek override etmeyiz.
+     */
+
+}
