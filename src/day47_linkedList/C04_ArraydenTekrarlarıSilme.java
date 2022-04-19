536e04103f9bb56f5839a058a5e82ad97e5a9b3b
Bahadır Koramaz <bkork1maz@gmail.com>
2022-04-19 19:06:02 +0300

    19.04.22

--- src/day47_linkedList/C04_ArraydenTekrarlarıSilme.java
+++ src/day47_linkedList/C04_ArraydenTekrarlarıSilme.java
@@ -11,19 +11,28 @@ public class C04_ArraydenTekrarlarıSilme {
         //uniq elementler oluşan bir array haline dönduren bir method oluşturun.
 
 
-        int arr[]={3,5,4,6,3,1,2,7,8,6,1,4,2,8};
+        Integer arr[]={3,5,4,6,3,1,2,7,8,6,1,4,2,8};
 
         arr=benzersizYap(arr);
 
     }
 
-    private static int[] benzersizYap(int[] arr) {
+    private static Integer[] benzersizYap(Integer[] arr) {
         Set<Integer>benzersizSet = new TreeSet<>();
         for (Integer each:arr
              ) {
             benzersizSet.add(each);
         }
         System.out.println(benzersizSet);
+        // Set de index yapısı yoktur,dolayısıyla forloop kullanamayız
+
+        Integer arrBenzersiz[]=new Integer[benzersizSet.size()];
+        int index = 0;
+        for (Integer each:benzersizSet
+             ) {
+            arr[index]=each;
+            index++;
+        }
 
         return arr;
     }
